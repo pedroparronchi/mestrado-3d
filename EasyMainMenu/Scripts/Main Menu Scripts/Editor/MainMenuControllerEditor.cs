dd6a95a02c411aef9b11f0cfea235ff4dfec97e0
Pedro Parronchi <pedrohenrique@univem.edu.br>
2020-04-21 16:52:24 -0300

    #add menu

---
+++ EasyMainMenu/Scripts/Main Menu Scripts/Editor/MainMenuControllerEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MainMenuController))]
+public class MainMenuControllerEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        
+
+        
+    }
+
+}
